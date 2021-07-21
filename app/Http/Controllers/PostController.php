bff130b74caf76f27eca83e22126fd41dc51c5db
Michael Nix <mpnix@protonmail.com>
2021-07-21 20:57:44 +0100

    Corrected updatepost method - removed unneeded square brackets

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,9 +60,7 @@ class PostController extends Controller
         }
 
         $post = Post::find($id);
-        $post->update([
-            $data
-        ]);
+        $post->update($data);
 
         return redirect('/blogposts');
         
